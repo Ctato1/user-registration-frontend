bc120532ecb0522d8e26777a3cf241cac017bfb9
Ctato1 <86792981+Ctato1@users.noreply.github.com>
2024-12-16 00:43:59 +0400

    add: error handling-pipe-form validation

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,24 +1,52 @@
-import { Component } from '@angular/core';
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {Component} from '@angular/core';
+import {AbstractControl, FormBuilder, ReactiveFormsModule, ValidatorFn, Validators} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {FirstKeyPipe} from "../../shared/pipes/first-key.pipe";
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule,FirstKeyPipe],
   templateUrl: './registration.component.html',
   styles: ``
 })
 export class RegistrationComponent {
-    constructor(public formBuilder:FormBuilder) {}
+  isSubmitted: boolean = false;
+  constructor(public formBuilder: FormBuilder) {
+  }
 
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): null => {
+    const password = control.get("password");
+    const confirmPassword = control.get("confirmPassword");
+
+    if(password && confirmPassword && password.value != confirmPassword.value){
+      confirmPassword?.setErrors({passwordMismatch:true});
+    }
+    else{
+      confirmPassword?.setErrors(null);
+    }
+
+
+
+    return null;
+  }
   form = this.formBuilder.group({
-    fullName:[''],
-    email:[''],
-    password:[''],
-    confirmPassword:[''],
-  })
+    fullName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required,
+      Validators.minLength(6),
+      Validators.pattern(/(?=.*[^a-zA-Z0-9 ])/)]],
+    confirmPassword: [''],
+  }, {validators: this.passwordMatchValidator})
 
-  onSubmit(){
+
+  onSubmit() {
+    this.isSubmitted = true;
     console.log(this.form.value)
+  }
+
+  hasDisplayableError(controlName:string):boolean{
+    const control = this.form.get(controlName);
+    return Boolean(control?.invalid) && (this.isSubmitted || Boolean(control?.touched));
   }
 }
